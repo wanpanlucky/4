5c4b5e97b459effa32db384917ca027a2c219cfe
wanpanlucky <1462607050@qq.com>
2016-05-30 18:23:01 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,16 @@
 
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+ var i,j;
+	var arr=new Array();
+	for(i in collection_a){
+		var f=collection_a[i].key;
+		for(j=0;j<object_b.value.length;j++)
+			if(f===object_b.value[j]){
+				arr.push(object_b.value[j]);
+				break;
+			}
+		}
+		return arr;
 }
 
 module.exports = collect_same_elements;
